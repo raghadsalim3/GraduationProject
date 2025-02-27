74ab4415f98f3d959b36276012ca781740fdb8f4
ZainaSalem <158177966+ZinaSalem123@users.noreply.github.com>
2025-02-27 05:24:18 +0300

    add_EmployeeAction_index

--- Backend/app/Http/Controllers/EmployeeController.php
+++ Backend/app/Http/Controllers/EmployeeController.php
@@ -12,10 +12,36 @@ class EmployeeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($token)
     {
-        //
+        //Give me all the employees they work in specified HealthCenter:
+        //Why the error is happend SQLSTATE[42S22]: Column not found: 1054 Unknown column 'remember_token' in 'where clause' 
+        //(Connection: mysql, SQL: select * from `persons` where `remember_token` = 3ytfegc462tfgg554FGfSEvbFG limit 1
+        // but I want to chek the remember_token in the user table
+
+        // $getPerson_id=$getUser->persons_id;
+        // $getPerson = Person::find($getPerson_id);
+        // return['persons_id'=>$getUser->persons_id];
+            $getPerson = User::find($token);
+            if($getPerson!=null){
+
+                $getPersonId=$getPerson->id;
+                $employee=Employee::find($getPersonId);
+                $gethealth_center_id=$employee->health_center_id;
+                $getAllEmployees=Employee::where('health_center_id',$gethealth_center_id)->get();
+    
+                $employeeCount = $getAllEmployees->count();
+                return [
+                        'employees' => $getAllEmployees,
+                        'count' => $employeeCount,
+                    ];
+            }else{
+                return response()->json([
+                    'message' => 'Not Found this user!'
+                ], 401);
+            }      
     }
+
 
     /**
      * Store a newly created resource in storage.
